4e2250a10f1bb29841d94afa1bf83e0957075704
Duke Do <91707869+stewwwwwww@users.noreply.github.com>
2024-05-21 17:39:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const workoutRoutes = require("./routes/workouts.js");
 const productRoutes = require("./routes/product.js");
 const researchRoutes = require("./routes/research.js")
 const articleRoutes = require("./routes/article.js")
+const faqRoutes = require("./routes/faq.js")
+const memberRoutes = require("./routes/member.js")
+const retailerRoutes = require("./routes/retailer.js")
+const joinUsRoutes = require("./routes/hiring.js")
 const mongoose = require("mongoose");
 
+app.use(cors());
 const app = express();
 //middleware
 app.use(express.json());
@@ -18,6 +24,10 @@ app.use("/api/workouts", workoutRoutes);
 app.use("/Products", productRoutes)
 app.use("/Researchs", researchRoutes)
 app.use("/Articles", articleRoutes)
+app.use("/Faqs", faqRoutes)
+app.use("/Members", memberRoutes)
+app.use("/Retailers", retailerRoutes)
+app.use("/JoinUs", joinUsRoutes)
 //connect to db
 mongoose
   .connect(process.env.MONGO_URI)
