2f5cd6fc58896e9fb0406ddff142de83be2cdf63
Duke Do <duc.do@smus.ca>
2024-05-25 17:58:18 +0700

    commit

---
+++ middleware/requireAuthentication.js
@@ -0,0 +1,19 @@
+jwt = require("jsonwebtoken")
+
+const requireAuthentication = (req, res, next) =>{
+    //verify authentication
+    const {authorization} = req.headers
+    if(!authorization){
+        return res.status(401).json({error: "Authorization token required"})
+    }
+    const token = authorization.split(" ")[1]
+
+    try{
+        jwt.verify(token, process.env.SECRET_TOKEN)
+    }catch (err){
+        console.log(err)
+        return res.status(401).json({error: "Request is not authorized"})
+
+    }
+}
+module.exports = requireAuthentication
--- server.js
+++ server.js
@@ -1,31 +1,42 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const workoutRoutes = require("./routes/workouts.js");
 const productRoutes = require("./routes/product.js");
-const researchRoutes = require("./routes/research.js")
-const articleRoutes = require("./routes/article.js")
-const faqRoutes = require("./routes/faq.js")
-const memberRoutes = require("./routes/member.js")
-const retailerRoutes = require("./routes/retailer.js")
-const joinUsRoutes = require("./routes/hiring.js")
+const researchRoutes = require("./routes/research.js");
+const articleRoutes = require("./routes/article.js");
+const faqRoutes = require("./routes/faq.js");
+const memberRoutes = require("./routes/member.js");
+const retailerRoutes = require("./routes/retailer.js");
+const joinUsRoutes = require("./routes/hiring.js");
 const mongoose = require("mongoose");
+const requireAuthentication = require("./middleware/requireAuthentication.js")
 
 const app = express();
-//middleware
+
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    optionsSuccessStatus: 200,
+  }),
+);
+
 app.use(express.json());
+//middleware
+app.use(requireAuthentication)
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
-//routes
+
 app.use("/api/workouts", workoutRoutes);
-app.use("/Products", productRoutes)
-app.use("/Researchs", researchRoutes)
-app.use("/Articles", articleRoutes)
-app.use("/Faqs", faqRoutes)
-app.use("/Members", memberRoutes)
-app.use("/Retailers", retailerRoutes)
-app.use("/JoinUs", joinUsRoutes)
+app.use("/Products", productRoutes);
+app.use("/Researchs", researchRoutes);
+app.use("/Articles", articleRoutes);
+app.use("/Faqs", faqRoutes);
+app.use("/Members", memberRoutes);
+app.use("/Retailers", retailerRoutes);
+app.use("/JoinUs", joinUsRoutes);
 //connect to db
 mongoose
   .connect(process.env.MONGO_URI)
