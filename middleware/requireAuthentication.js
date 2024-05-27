f6b0d078376d9aae547ede99ead5264f9260bd00
Duke Do <duc.do@smus.ca>
2024-05-27 19:12:11 +0700

    commit

--- middleware/requireAuthentication.js
+++ middleware/requireAuthentication.js
@@ -1,4 +1,4 @@
-jwt = require("jsonwebtoken");
+jwt = require("jsonwebtoken")
 
 // const requireAuthentication = (req, res, next) =>{
 //     //verify authentication
@@ -16,39 +16,27 @@ jwt = require("jsonwebtoken");
 //         console.log(token)
 
 //         return res.status(401).json({error: "Request is not authorized"})
-
+        
 //     }
 // }
-const generateToken = (user) => {
-  // Ensure you have a user object with a unique identifier
-  return jwt.sign(
-    { userId: user._id }, // Payload
-    process.env.SECRET_KEY, // Secret key from .env file
-    { expiresIn: "1h" }, // Token expiration time
-  );
-};
 
-// Example usage
-const user = { _id: "12345" }; // Replace with actual user object from your database
-const token = generateToken(user);
-console.log("Generated Token:", token);
-app.use(requireAuthentication);
 const requireAuthentication = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (!token) {
-    console.log("No token provided");
-    return res.status(401).json({ error: "Authorization token required" });
-  }
-
-  jwt.verify(token, process.env.SECRET_TOKEN, (err) => {
-    if (err) {
-      console.log("Token verification failed", err);
-      return res.status(403).json({ error: "Request is not authorized" });
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+  
+    if (!token) {
+      console.log("No token provided");
+      return res.status(401).json({ error: "Authorization token required" });
     }
+  
+    jwt.verify(token, process.env.SECRET_TOKEN, (err) => {
+      if (err) {
+        console.log("Token verification failed", err);
+        return res.status(403).json({ error: "Request is not authorized" });
+      }
+  
+      next();
+    });
+  };
+module.exports = requireAuthentication
 
-    next();
-  });
-};
-module.exports = requireAuthentication;
