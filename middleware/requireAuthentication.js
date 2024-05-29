e2b45ecfe0c52d66ced8a373c9d474cd72337543
Duke Do <duc.do@smus.ca>
2024-05-30 00:09:19 +0700

    c

--- middleware/requireAuthentication.js
+++ middleware/requireAuthentication.js
@@ -21,7 +21,7 @@ jwt = require("jsonwebtoken")
 // }
 
 const requireAuthentication = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
+    const authHeader = req.headers['Authorization'];
     const token = authHeader && authHeader.split(' ')[1];
   
     if (!token) {
