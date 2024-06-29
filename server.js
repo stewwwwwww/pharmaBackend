352c7c18975d9b79eb8f6b1127ecc0994090e695
Duke Do <duc.do@smus.ca>
2024-06-29 11:46:12 +0700

    m

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://phuongminhpharma.netlify.app",
-    accessControlAllowOrigin: "https://phuongminhpharma.netlify.app",
+    origin: "https://phuongminhpharma.netlify.app/",
+    accessControlAllowOrigin: "https://phuongminhpharma.netlify.app/",
   }),
 );
 //middleware
