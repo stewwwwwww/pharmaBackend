85003d824c3f5473a3736f20694d7f64456bfec6
Duke Do <duc.do@smus.ca>
2024-07-05 00:12:46 +0700

    m

--- authServer.js
+++ authServer.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.post("/token", (req, res) => {
   if ((req.headers["authorization"] = `Bearer ${process.env.SECRET_TOKEN}`)) {
-    const accessToken = jwt.sign(user, process.env.SECRET_TOKEN, {
+    const accessToken = jwt.sign(process.env.SECRET_TOKEN, {
       expiresIn: "600s",
     });
     res.json({ accessToken: accessToken });
