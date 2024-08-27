583242ebc772b8c6e19903dfe786cab18906eb0d
Duke Do <duc.do@smus.ca>
2024-08-27 20:32:40 +0700

    m

--- authServer.js
+++ authServer.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allowed-Origin", "https://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
 });
 app.use(cors());
 app.post("/token", (req, res) => {
