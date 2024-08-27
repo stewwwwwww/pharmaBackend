1cfaf148bb73cba2f53b48d7873e50c05b9f07c6
Duke Do <duc.do@smus.ca>
2024-08-27 20:31:57 +0700

    m

--- authServer.js
+++ authServer.js
@@ -8,13 +8,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allowed-Origin", "https://localhost:3000");
 });
-app.use(
-  cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-  }),
-);
+app.use(cors());
 app.post("/token", (req, res) => {
   if (req.headers["authorization"] === `Bearer ${process.env.SECRET_TOKEN}`) {
     const accessToken = jwt.sign({ user: "admin" }, process.env.SECRET_TOKEN, {
