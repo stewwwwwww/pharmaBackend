33e1fad5474951749f68bcf7715834efb9b1b136
Duke Do <duc.do@smus.ca>
2024-09-05 04:10:05 +0700

    m

--- server.js
+++ server.js
@@ -18,48 +18,48 @@ const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "https://palegreen-sardine-650982.hostingersite.com",
-// ];
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (allowedOrigins.includes(origin) || !origin) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     methods: "GET,PUT,POST,DELETE,OPTIONS,PATCH", // allow these methods
-//     allowedHeaders: "Content-Type, Authorization", // allow these headers
-//   }),
-// );
-// app.use((req, res, next) => {
-//   const origin = req.headers.origin;
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://palegreen-sardine-650982.hostingersite.com",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,PUT,POST,DELETE,OPTIONS,PATCH", // allow these methods
+    allowedHeaders: "Content-Type, Authorization", // allow these headers
+  }),
+);
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
 
-//   // Check if the origin is allowed
-//   if (allowedOrigins.includes(origin)) {
-//     res.header("Access-Control-Allow-Origin", origin);
-//   } else {
-//     // If the origin is not allowed, return a 403 response
-//     return res.status(403).json({ message: "Forbidden: Access is denied." });
-//   }
+  // Check if the origin is allowed
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  } else {
+    // If the origin is not allowed, return a 403 response
+    return res.status(403).json({ message: "Forbidden: Access is denied." });
+  }
 
-//   // Set other CORS headers
-//   res.header(
-//     "Access-Control-Allow-Methods",
-//     "GET,PUT,POST,DELETE,OPTIONS,PATCH",
-//   );
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  // Set other CORS headers
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,OPTIONS,PATCH",
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-//   // Handle preflight requests
-//   if (req.method === "OPTIONS") {
-//     return res.status(200).end();
-//   }
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
-//   next();
-// });
+  next();
+});
 //middleware
 app.use((req, res, next) => {
   console.log(req.path, req.method);
