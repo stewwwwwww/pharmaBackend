e783b9a84687a33f8a9557b2525146823c356b2c
Duke Do <duc.do@smus.ca>
2024-09-05 01:39:52 +0700

    m

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://phuongminhpharma.netlify.app",
+  "https://palegreen-sardine-650982.hostingersite.com/",
 ];
 app.use(
   cors({
